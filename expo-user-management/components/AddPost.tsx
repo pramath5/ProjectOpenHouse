03ef7a8c1da98a84cf594005d564ca463038081f
Pranava116 <pranavagrao116@gmail.com>
2025-06-10 10:31:23 +0530

    Added DB yet to fix the date time issue

--- expo-user-management/components/AddPost.tsx
+++ expo-user-management/components/AddPost.tsx
@@ -4,28 +4,51 @@ import { Alert, TextInput } from "react-native";
 import { Button } from "react-native";
 import { supabase } from "../lib/supabase";
 import { View, Text, StyleSheet } from "react-native";
-
-export default function Tab() {
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+export default function Tab({ navigation }: any) {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [venue, setVenue] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState(new Date(1598051730000));
   const [club, setClub] = useState("");
   async function Submit() {
     const { data, error } = await supabase
       .from("posts")
-      .insert([{ title: title, desc: desc, venue: venue, club: club }])
+      .insert([
+        { title: title, desc: desc, venue: venue, club: club, date: date },
+      ])
       .select();
     console.log(error);
+    navigation.navigate("Admin");
+    setTitle("");
+    setDesc("");
+    setVenue("");
     Alert.alert("Submitted successfully");
   }
+  const onChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+  };
+  const showMode = (currentMode: any) => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange,
+      mode: currentMode,
+    });
+  };
+  const showDatepicker = () => {
+    showMode("date");
+  };
+  let date_string = date.toString();
   return (
     <View style={styles.container}>
       <Input placeholder="Tile" onChangeText={(text) => setTitle(text)} />
       <Input placeholder="Club" onChangeText={(text) => setClub(text)} />
       <Input placeholder="Desc" onChangeText={(text) => setDesc(text)} />
       <Input placeholder="Venue" onChangeText={(text) => setVenue(text)} />
-      <Input placeholder="date" onChangeText={(text) => setDate(text)} />
+      <Input placeholder="Date" value={date_string} />
+      <Button onPress={showDatepicker} title="Show date picker!" />
       <Button title="Submit" onPress={() => Submit()} />
     </View>
   );
